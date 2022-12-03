b0846c2445e48fa69031b4bfd61db36f65604d3b
sunkachunchris <72062731+sunkachunchris@users.noreply.github.com>
2022-12-04 00:48:03 +0800

    test

--- app/src/main/java/com/example/aslcodingtestproject/view/FirstFragment.kt
+++ app/src/main/java/com/example/aslcodingtestproject/view/FirstFragment.kt
@@ -48,6 +48,7 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
         getPhoto()
+
     }
 
     private fun getPhoto() {
